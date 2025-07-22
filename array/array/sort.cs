370ff2bf6e70ba96608b04318b74dbdf9f8ced3d
abantikalama <abantikalama10@gmail.com>
2025-07-22 13:41:14 +0545

    added sorting of array

---
+++ array/array/sort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+internal class SortExample
+{
+    static void Main()
+    {
+        int[] x = { 4, 2, 1, 3 };
+
+        Array.Sort(x); // Sort the entire array in-place
+
+        foreach (int i in x)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
