6d4c44182c7c0259949e3a370dc3a998a75828d0
abantikalama <abantikalama10@gmail.com>
2025-07-22 15:23:23 +0545

    checked unique element in array

---
+++ array/array/uniqueelement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    
+
+class UniqueElements
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 2, 2, 3, 4, 4, 5 };
+
+            Console.WriteLine("Unique elements in the array are:");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isUnique = true;
+
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (i != j && arr[i] == arr[j])
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+
+                if (isUnique)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+        }
+    }
+
+}
+
